1a18f69e288fe6f641d8bdaecacaefdb31788aa2
Michael <michael.bretagne@gmail.com>
2019-01-13 10:40:46 -0800

    Apply standardization and optimize learning rate

--- linear-regression.js
+++ linear-regression.js
@@ -3,17 +3,15 @@ const _ = require("lodash");
 
 class LinearRegression {
   constructor(features, labels, options) {
-    this.features = tf.tensor(features);
+    this.features = this.processFeatures(features);
     this.labels = tf.tensor(labels);
+    this.mseHistory = [];
 
-    this.features = tf
-      .ones([this.features.shape[0], 1])
-      .concat(this.features, 1);
     this.options = Object.assign(
       { learningRate: 0.1, iterations: 1000 },
       options,
     );
-    this.weights = tf.zeros([2, 1]);
+    this.weights = tf.zeros([this.features.shape[1], 1]);
   }
 
   gradientDescent() {
@@ -30,15 +28,16 @@ class LinearRegression {
 
   train() {
     for (let i = 0; i < this.options.iterations; i++) {
+      console.log(this.options.learningRate);
       this.gradientDescent();
+      this.recordMSE();
+      this.updateLearningRate();
     }
   }
 
   test(testFeatures, testLabels) {
-    testFeatures = tf.tensor(testFeatures);
+    testFeatures = this.processFeatures(testFeatures);
     testLabels = tf.tensor(testLabels);
-
-    testFeatures = tf.ones([testFeatures.shape[0], 1]).concat(testFeatures, 1);
 
     const predictions = testFeatures.matMul(this.weights);
 
@@ -55,6 +54,53 @@ class LinearRegression {
       .get();
 
     return 1 - res / tot;
+  }
+
+  processFeatures(features) {
+    features = tf.tensor(features);
+
+    if (this.mean && this.variance) {
+      features = features.sub(this.mean).div(this.variance.pow(0.5));
+    } else {
+      features = this.standardize(features);
+    }
+
+    features = tf.ones([features.shape[0], 1]).concat(features, 1);
+
+    return features;
+  }
+
+  standardize(features) {
+    const { mean, variance } = tf.moments(features, 0);
+
+    this.mean = mean;
+    this.variance = variance;
+
+    return features.sub(mean).div(variance.pow(0.5));
+  }
+
+  recordMSE() {
+    const mse = this.features
+      .matMul(this.weights)
+      .sub(this.labels)
+      .pow(2)
+      .sum()
+      .div(this.features.shape[0])
+      .get();
+
+    this.mseHistory.unshift(mse);
+  }
+
+  updateLearningRate() {
+    if (this.mseHistory.length < 2) {
+      return;
+    }
+
+    if (this.mseHistory[0] > this.mseHistory[1]) {
+      this.options.learningRate /= 2;
+    } else {
+      this.options.learningRate *= 1.05;
+    }
   }
 }
 
